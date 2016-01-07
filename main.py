77de30cadba5383aa3a186b82943da173dfa17fa
Hyjal <261031496@qq.com>
2016-01-07 14:50:57 +0800

    加入熔断boom

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 #coding=utf8
+import time
 from PIL import Image
 from sinajs import sinastock
 
+def SHIndexMon():
+    while True:
+        detail = data.Getsh()
+        print detail.indexname,detail.currentindex,detail.indexincrease,detail.indexchangerate
+        if abs(detail.indexincrease) > 5 and abs(detail.indexincrease) < 7:
+            time.sleep(890)
+            print 'boom!'
+        if abs(detail.indexincrease) > 7:
+            print 'boom!boom!boom!'
+            print 'game over!'
+            break
+        time.sleep(10)
+        
+
 if __name__ == '__main__':
     data = sinastock()
-    detail = data.GetDataByStockCode('sh601006')
-    #print detail.name,detail.currentprice
-    data.GetDailyKLineByStockCode('sh601006')
-    
+    #detail = data.GetDataByStockCode('sh601006')
+    #detail = data.GetDailyKLineByStockCode('sh601006')
+    #detail = data.Getsh()
+    SHIndexMon()
